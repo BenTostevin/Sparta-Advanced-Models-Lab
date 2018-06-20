fe9776b33087d28452042d51ca0bf08250f6fd80
Tech-A34 <Tech-A34@Tech-A34s-MacBook-Air.local>
2018-06-20 11:20:46 +0100

    added authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_100310) do
+ActiveRecord::Schema.define(version: 2018_06_20_101046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "dob"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "country_id"
+  end
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
 3.times do
   Country.create(name: Faker::Address.country)
 end
+
+5.times do
+  random_id = rand(1..Country.all.length)
+  Author.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, dob: Faker::Date.birthday(18,80), country_id: random_id)
+end
